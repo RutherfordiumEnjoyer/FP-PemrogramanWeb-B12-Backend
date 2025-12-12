7847ef99a4c7b4ee1c270ab12f12322bdd9f5df3
RutherfordiumEnjoyer <ranuajiabyan@gmail.com>
2025-12-12 16:59:48 +0700

    Fix lint

--- src/api/game/game-list/game-list.router.ts
+++ src/api/game/game-list/game-list.router.ts
@@ -2,12 +2,12 @@
 /* eslint-disable import/no-default-export */
 import { Router } from 'express';
 
+import airplaneRouter from './airplane/airplane.router';
 import { AnagramController } from './anagram/anagram.controller';
 import { PairOrNoPairController } from './pair-or-no-pair/pair-or-no-pair.controller';
+import { QuizController } from './quiz/quiz.controller';
 import { SpeedSortingController } from './speed-sorting/speed-sorting.controller';
 import { TypeSpeedController } from './type-speed/type-speed.controller';
-import { QuizController } from './quiz/quiz.controller';
-import airplaneRouter from './airplane/airplane.router';
 
 const GameListRouter = Router();
 
