6cba8629f5ac0a5100b52ca927119fda63ea08ba
RutherfordiumEnjoyer <ranuajiabyan@gmail.com>
2025-12-14 11:04:58 +0700

    lint fix

--- src/api/game/game-list/game-list.router.ts
+++ src/api/game/game-list/game-list.router.ts
@@ -4,6 +4,7 @@ import { Router } from 'express';
 
 import airplaneRouter from './airplane/airplane.router';
 import { AnagramController } from './anagram/anagram.controller';
+import { MazeChaseController } from './maze-chase/maze-chase.controller';
 import { PairOrNoPairController } from './pair-or-no-pair/pair-or-no-pair.controller';
 import { QuizController } from './quiz/quiz.controller';
 import { SlidingPuzzleController } from './sliding-puzzle/sliding-puzzle.controller';
@@ -12,16 +13,17 @@ import { SpinTheWheelController } from './spin-the-wheel/spin-the-wheel.controll
 import { TrueOrFalseController } from './true-or-false/true-or-false.controller';
 import { TypeSpeedController } from './type-speed/type-speed.controller';
 
-const gameListRouter = Router();
+const GameListRouter = Router();
 
-gameListRouter.use('/quiz', QuizController);
-gameListRouter.use('/sliding-puzzle', SlidingPuzzleController);
-gameListRouter.use('/speed-sorting', SpeedSortingController);
-gameListRouter.use('/anagram', AnagramController);
-gameListRouter.use('/pair-or-no-pair', PairOrNoPairController);
-gameListRouter.use('/type-speed', TypeSpeedController);
-gameListRouter.use('/airplane', airplaneRouter);
-gameListRouter.use('/spin-the-wheel', SpinTheWheelController);
-gameListRouter.use('/true-or-false', TrueOrFalseController);
+GameListRouter.use('/quiz', QuizController);
+GameListRouter.use('/maze-chase', MazeChaseController);
+GameListRouter.use('/sliding-puzzle', SlidingPuzzleController);
+GameListRouter.use('/speed-sorting', SpeedSortingController);
+GameListRouter.use('/anagram', AnagramController);
+GameListRouter.use('/pair-or-no-pair', PairOrNoPairController);
+GameListRouter.use('/type-speed', TypeSpeedController);
+GameListRouter.use('/airplane', airplaneRouter);
+GameListRouter.use('/spin-the-wheel', SpinTheWheelController);
+GameListRouter.use('/true-or-false', TrueOrFalseController);
 
-export { gameListRouter };
+export default GameListRouter;
